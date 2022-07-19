31ca8b1e0bcbe0653d40a6eb3c003b3c1cf20b56
Pablo Zapata <pablo.zapata.dev@gmail.com>
2022-07-19 23:20:26 +0200

    :wrench: Add blipPosition in config

--- config.lua
+++ config.lua
@@ -5,5 +5,7 @@ Config = {
         position = vector3(1088.1834, -3099.3457, -38.9999),
         sortie = vector3(875.89, -1576.85, 30.81)
     },
+
+    blipPosition = vector3(875.89, -1576.85, 30.81)
 }
 
